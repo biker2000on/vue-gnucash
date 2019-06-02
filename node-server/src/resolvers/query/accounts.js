031cc358b57bbf1c48a345a941d517db1c90d6c8
biker2000on <bikerhiker10@gmail.com>
2019-06-02 18:19:13 +0800

    added depth to account schema with CTE

--- node-server/src/resolvers/query/accounts.js
+++ node-server/src/resolvers/query/accounts.js
@@ -1,7 +1,12 @@
 const accountsResolver = {
   Query: {
     async accounts (root, args, {knex}) {
-      return knex('accounts').select()
+      // return knex('accounts').select()
+      return knex.withRecursive('ancestors', (qb) => {
+        qb.select('accounts.*', knex.raw('0 as depth')).from('accounts').where('guid', 'fd4dd79886327b270a0fa8efe6a07972').unionAll((qb) => {
+          qb.select('accounts.*', knex.raw('ancestors.depth + 1 as depth')).from('accounts').join('ancestors','ancestors.guid','accounts.parent_guid')
+        })
+      }).select().from('ancestors')
     },
   }
 }
