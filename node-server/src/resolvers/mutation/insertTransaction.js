8232475ae1e15f7609de52890088f1f5f6749abb
biker2000on <bikerhiker10@gmail.com>
2019-06-20 19:01:39 +0800

    insertTransaction with Input type

--- node-server/src/resolvers/mutation/insertTransaction.js
+++ node-server/src/resolvers/mutation/insertTransaction.js
@@ -3,60 +3,63 @@ const moment = require('moment')
 
 const insertTransactionResolver = {
   Mutation: {
-    async insertTransaction (root, args, {knex}) {
-      const transGuid = uuidv4().replace(/-/g,'')
+    async insertTransaction(root, args, {
+      knex
+    }) {
+      const transGuid = uuidv4().replace(/-/g, '')
+      console.log(args.splits)
       // Using trx as a query builder:
-      knex.transaction(function(trx) {
-        const splits = args.account_guids.map((c,i) => {
-          return {
-            guid: uuidv4().replace(/-/g,''),
-            account_guid: c,
-            value_num: args.values[i] * 100,
-            quantity_num: args.quantities ? args.quantities[i] * 100 : args.values[i] * 100,
-            value_denom: 100,
-            quantity_denom: 100,
-            tx_guid: transGuid,
-            reconcile_state: "n", // not reconciled on creation
-            reconcile_date: "1970-01-01 00:00:00", // default reconcile date
-            memo: args.memos ? args.memos[i] : '',
-            action: args.actions ? args.actions[i] : '', // [Buy, Increase, Decrease, Sell] possible options
+      knex.transaction(function (trx) {
+          const splits = args.splits.map((c, i) => {
+            return {
+              guid: uuidv4().replace(/-/g, ''),
+              account_guid: c.account_guid,
+              value_num: c.value * 100,
+              quantity_num: c.quantity ? c.quantity * 100 : c.value * 100,
+              value_denom: 100,
+              quantity_denom: 100,
+              tx_guid: transGuid,
+              reconcile_state: "n", // not reconciled on creation
+              reconcile_date: "1970-01-01 00:00:00", // default reconcile date
+              memo: c.memo ? c.memo : '',
+              action: c.action ? c.action : '', // [Buy, Increase, Decrease, Sell] Actions enum
+            }
+          })
+
+          const transaction = {
+            guid: transGuid,
+            description: args.description,
+            post_date: moment(args.post_date).format('YYYY-MM-DD kk:mm:ss'), // yyyy-mm-dd hh:mm:ss kk is 24 hours
+            enter_date: moment().format('YYYY-MM-DD kk:mm:ss'),
+            currency_guid: "6bb5a4a2b2390043f8517de6cff31f5f", // USD GUID hardcoded
+            num: "", // default is "" Text(2048)
           }
-        })
 
-        const transaction = {
-          guid: transGuid, 
-          description: args.description, 
-          post_date: moment(args.post_date).format('YYYY-MM-DD kk:mm:ss'), // yyyy-mm-dd hh:mm:ss kk is 24 hours
-          enter_date: moment().format('YYYY-MM-DD kk:mm:ss'),
-          currency_guid: "6bb5a4a2b2390043f8517de6cff31f5f", // USD GUID hardcoded
-          num: "", // default is "" Text(2048)
-        }
-
-        return trx
-          .insert(transaction)
-          .into('transactions')
-          .then(function() {
-            return trx('splits').insert(splits);
+          return trx
+            .insert(transaction)
+            .into('transactions')
+            .then(function () {
+              return trx('splits').insert(splits);
             });
-      })
-      .then(function(inserts) {
-        console.log(inserts);
-      })
-      .catch(function(error) {
-        // If we get here, that means that neither the 'Old Books' catalogues insert,
-        // nor any of the books inserts will have taken place.
-        console.error(error);
-      });
+        })
+        .then(function (inserts) {
+          console.log(inserts);
+        })
+        .catch(function (error) {
+          // If we get here, that means that neither the 'Old Books' catalogues insert,
+          // nor any of the books inserts will have taken place.
+          console.error(error);
+        });
       // console.log("args", args)
       // console.log('uuid', uuidv4().replace(/-/g,''))
       // const insert = await knex('transactions').where('guid',args.guid).update({...args, enter_date: new Date().toISOString()})
       // console.log("update", update) // returns number of rows updated
-      const transaction = await knex('transactions').select().where('guid',transGuid)
+      const transaction = await knex('transactions').select().where('guid', transGuid)
       return transaction[0]
     }
   }
 }
 
 module.exports = {
-  insertTransactionResolver  
+  insertTransactionResolver
 }
--- node-server/src/typeDefs/mutation.js
+++ node-server/src/typeDefs/mutation.js
@@ -3,25 +3,45 @@ const { gql } = require('apollo-server')
 const mutation = gql`
   type Mutation {
     """
-    Update a transactions metadata and not the splits.
+    Update a transactions with splits. Must include all splits relevant to transaction for update otherwise you will get a validation error. 
     """
     updateTransaction(
       guid: String!, 
       description: String, 
       post_date: String
+      splits: [UpdateSplitInput]
     ): Transaction
+
     """
-    Insert a new transaction with multiple splits.
+    Insert a transaction with multiple splits
     """
     insertTransaction(
-      description: String! "transaction description", 
-      post_date: String!, 
-      account_guids: [ID!]! "array of all account_guids for all splits", 
-      values: [Float!]!, 
-      quantities: [Float], 
-      memos: [String],
-      actions: [Actions],
+      description: String!
+      post_date: String!
+      splits: [InsertSplitInput]
     ): Transaction
+  }
+
+  input InsertSplitInput {
+    "account_guid that this split maps to"
+    account_guid: ID!
+    "Value of transaction"
+    value: Float!
+    "Quantity of transaction. Will equal value on same-same currency transaction. Will not equal value on stock or multi-currency transactions."
+    quantity: Float
+    "comment / memo for the split. This is in addition to the description on the transaction"
+    memo: String
+    "Enum of possible split actions related to stock / mutual fund movements."
+    action: Actions
+  }
+
+  input UpdateSplitInput {
+    guid: ID!
+    account_guid: ID!
+    value: Float!
+    quantity: Float
+    memo: String
+    action: Actions
   }
 `
 
